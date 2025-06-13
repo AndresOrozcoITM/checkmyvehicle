1cd5c6f92c6869e3e3359197b8cdc7e91bab5816
Andres <andresorozco212819@correo.itm.edu.co>
2025-06-12 22:04:31 -0500

    Agregue el dumb de la bd

---
+++ db/dumb.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS checkmyvehicle;
+USE checkmyvehicle;
+
+CREATE TABLE IF NOT EXISTS vehicles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  placa VARCHAR(20) UNIQUE,
+  descripcion VARCHAR(100),
+  mid VARCHAR(50)
+);
